03c84178e1a565366ab6409d5fdc4e7f1b0b9445
Nikita <nikitaseliverstov@gmail.com>
2019-02-24 22:16:34 +0300

    Fix viewController

--- Local Notifications/ViewController.swift
+++ Local Notifications/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal() {
+        
+    }
 }
 
